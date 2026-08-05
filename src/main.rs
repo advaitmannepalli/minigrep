aae1dbb40f30d8cae96df5a3b6e6cf030162700c
Advait Mannepalli <advait@Advaits-MacBook-Air.local>
2026-08-04 19:59:56 -0500

    set up lib, refactor, error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::search;
 
 fn main() {
     println!("---Hello, minigrep!---");
 
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_name = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_name);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_name);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 
-    let contents = fs::read_to_string(file_name)
-        .expect("Should have been able to read the file");
+    // run(config);
+}
 
-    println!("With text: \n{}", contents);
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_name)?;
+        // .expect("Should have been able to read the file");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config {query, file_name})
+    }
 }
