6dab60968dfed0fa51af528617741d73a1f4cd70
Advait Mannepalli <advait@Advaits-MacBook-Air.local>
2026-08-04 19:03:45 -0500

    Reads and prints a  file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    println!("---Hello, minigrep!---");
 
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
@@ -11,4 +12,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_name);
+
+    let contents = fs::read_to_string(file_name)
+        .expect("Should have been able to read the file");
+
+    println!("With text: \n{}", contents);
 }
